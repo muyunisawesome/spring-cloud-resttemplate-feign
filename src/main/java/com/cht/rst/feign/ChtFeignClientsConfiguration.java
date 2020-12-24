76d37f41553e80ee863d13b47f7a41c9e0bf6275
chenght <chenght@t3go.cn>
2020-12-24 18:50:04 +0800

    fix old httpconvert don't contains new httpconvert occur add exception because of Lists.asList

--- src/main/java/com/cht/rst/feign/ChtFeignClientsConfiguration.java
+++ src/main/java/com/cht/rst/feign/ChtFeignClientsConfiguration.java
@@ -18,7 +18,7 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -61,8 +61,7 @@ public class ChtFeignClientsConfiguration {
 
             if (INITED.compareAndSet(false, true)) {
                 List<HttpMessageConverter<?>> oldMessageConverters = restTemplate.getMessageConverters();
-                List<HttpMessageConverter<?>> newMessageConverters =
-                        Arrays.asList(new HttpMessageConverter<?>[oldMessageConverters.size()]);
+                List<HttpMessageConverter<?>> newMessageConverters = new ArrayList<>(oldMessageConverters);
                 Collections.copy(newMessageConverters, oldMessageConverters);
                 for (HttpMessageConverter<?> messageConverter : this.messageConverters.getObject().getConverters()) {
                     boolean has = false;
